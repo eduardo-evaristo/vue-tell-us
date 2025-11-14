8d3ca4ff9266a3057dede2743335b0ccace9c187
Eduardo Evaristo <eduardo.evaristo.am@gmail.com>
2025-11-14 17:00:29 -0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { House, ListVideo } from 'lucide-vue-next'
+</script>
+
+<template>
+  <nav class="bg-black flex justify-around p-4">
+    <div class="px-10 py-5">
+      <House color="white" />
+    </div>
+    <div class="px-10 py-5">
+      <ListVideo color="white" />
+    </div>
+  </nav>
+</template>
